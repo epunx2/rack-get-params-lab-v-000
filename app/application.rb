a003c1003dab7b172aa170e0a6a2c03dfd8b309a
Eric Chrobak <eric@ericchrobak.com>
2018-09-01 03:30:09 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -2,6 +2,8 @@ class Application
 
   @@items = ["Apples","Carrots","Pears"]
 
+  @@cart = []
+
   def call(env)
     resp = Rack::Response.new
     req = Rack::Request.new(env)
@@ -13,6 +15,17 @@ class Application
     elsif req.path.match(/search/)
       search_term = req.params["q"]
       resp.write handle_search(search_term)
+    elsif req.path.match(/cart/)
+      @@cart.each do |item|
+        resp.write "#{item}\n"
+      end
+    elsif req.path.match(/add/)
+      item = req.params["get"]
+      if @@item.include(item)
+        resp.write "Error!! This item is not available please add another item."
+      else
+        @@cart << item
+      end
     else
       resp.write "Path Not Found"
     end
